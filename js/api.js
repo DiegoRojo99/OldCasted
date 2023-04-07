35a61737578e9e51f605aa9753bd0dd9b1a4c985
Diego Rojo <Diego99rojo@gmail.com>
2023-04-07 13:57:48 +0100

    Game works and movies are from popular

--- js/api.js
+++ js/api.js
@@ -1,17 +1,21 @@
 const apiKey = '409703fb44ff5c9499d81e5855ecf46b';
 const baseUrl = 'https://api.themoviedb.org/3';
 let actors = [], numberActors=0;
+let movieIds=[];
+let movieTitle="";
 
 // Get a random movie from the discover endpoint
 function getRandomMovie() {
-    const endpoint = `${baseUrl}/discover/movie`;
-    let randomPage=Math.floor(Math.random() * 100) + 1;
+    const endpoint = `${baseUrl}/movie/popular`;
+    let randomPage=Math.floor(Math.random() * 5) + 1;
     const queryParams = `api_key=${apiKey}&include_adult=false&include_video=false&page=${randomPage}`;
     const url = `${endpoint}?${queryParams}`;
     fetch(url)
     .then(response => response.json())
     .then(data => {
         let movie=data.results[Math.floor(Math.random() * data.results.length)];
+        movieTitle=movie.title;
+        console.log(movieTitle);
         getCast(movie.id);
     });
 }
@@ -56,7 +60,26 @@ function showActors(){
     numberActors++;
 }
 
+function checkGuess(){
+  
+  const guessInput = document.getElementById('movie-search'); 
+  const userGuess = guessInput.value;
+  // Compare the user's guess to the movie title
+  if (userGuess.toLowerCase() === movieTitle.toLowerCase()) {
+    // The guess is correct
+    window.alert('Congratulations, you guessed the movie!');
+  } else {
+    // The guess is incorrect
+    console.log('Sorry, that is not the correct movie.');
+    showActors();
+  }
+
+  // Clear the input field
+  guessInput.value = '';
+
+}
+
 getRandomMovie();
 
 let actorButton = document.getElementById("actor-button");
-actorButton.addEventListener('click',showActors);
+actorButton.addEventListener('click',checkGuess);
